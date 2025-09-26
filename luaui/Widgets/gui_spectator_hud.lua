4b6c4cb907cfdd7180f275b8ad48e3fcac337ea4
DESKTOP-1QD0O9D\William <elesurge1392@gmail.com>
2025-09-26 16:53:29 -0500

    Revert "Add isCommanderArmy toggle to Spectator HUD"
    This reverts commit 546a9e72ff0a88c5924a962fc6b759a4ea601a0e.

--- luaui/Widgets/gui_spectator_hud.lua
+++ luaui/Widgets/gui_spectator_hud.lua
@@ -204,7 +204,6 @@ local settings = {
 	-- this table is used only when widgetConfig is set to custom
 	metricsEnabled = {},
 	oneTimeEcostatsEnableDone = false,
-	isCommanderArmy = true,
 }
 
 local metricKeys = {
@@ -270,7 +269,6 @@ end
 
 local function buildUnitDefs()
 	local function isCommander(unitDefID, unitDef)
-		--Spring.Echo('hue', unitDef.customParams.iscommander) ty badosu
 		return unitDef.customParams.iscommander
 	end
 
@@ -287,13 +285,8 @@ local function buildUnitDefs()
 	end
 
 	local function isArmyUnit(unitDefID, unitDef)
-		--Spring.Echo("Spectator HUD", settings)
-		--Spring.Log("Spectator HUD", LOG.WARNING, (isCommander(unitDefID, unitDef))) 
-		-- anything with a least one weapon and speed above zero is considered an army unit	
-		-- if isCommanderArmy is false, we ignore commander units for determining army
-		if (settings.isCommanderArmy == true) then return unitDef.weapons and (#unitDef.weapons > 0) and unitDef.speed and (unitDef.speed > 0)
-		else return (not isCommander(unitDefID, unitDef)) and unitDef.weapons and (#unitDef.weapons > 0) and unitDef.speed and (unitDef.speed > 0)
-		end
+		-- anything with a least one weapon and speed above zero is considered an army unit
+		return unitDef.weapons and (#unitDef.weapons > 0) and unitDef.speed and (unitDef.speed > 0)
 	end
 
 	local function isDefenseUnit(unitDefID, unitDef)
@@ -348,10 +341,6 @@ local function buildUnitDefs()
 	end
 end
 
-local function deleteUnitDefs()
-	unitDefsToTrack = {}
-end
-
 local function addToUnitCache(teamID, unitID, unitDefID)
 	local function addToUnitCacheInternal(cache, teamID, unitID, value)
 		if unitCache[teamID][cache] then
@@ -396,10 +385,6 @@ local function addToUnitCache(teamID, unitID, unitDefID)
 		addToUnitCacheInternal("economyBuildings", teamID, unitID,
 					   unitDefsToTrack.economyBuildingDefs[unitDefID])
 	end
-end
-
-local function deleteUnitCache()
-	unitCache = {}
 end
 
 local function removeFromUnitCache(teamID, unitID, unitDefID)
@@ -699,8 +684,6 @@ local function buildMetricsEnabled()
 			end
 		elseif settings.widgetConfig >= metric.configLevel then
 			addMetric = true
-		elseif settings.isCommanderArmy then
-			addMetric = false
 		end
 
 		if addMetric then
@@ -1881,8 +1864,6 @@ local function init()
 end
 
 local function deInit()
-	deleteUnitDefs()
-	deleteUnitCache()
 	deleteMetricDisplayLists()
 	deleteKnobVAO()
 	deleteTextures()
@@ -1912,15 +1893,6 @@ function widget:Initialize()
 	end
 	WG["spectator_hud"].setWidgetSize = function(value)
 		settings.widgetScale = value
-		reInit()
-	end
-
-	WG["spectator_hud"].getIsCommanderArmy = function()
-		return settings.isCommanderArmy
-	end
-
-	WG["spectator_hud"].setIsCommanderArmy = function(value)
-		settings.isCommanderArmy = value
 		reInit()
 	end
 
@@ -2171,8 +2143,6 @@ function widget:GetConfigData()
 		result.metricsEnabled[metric] = settings.metricsEnabled[metric]
 	end
 
-	result.isCommanderArmy = settings.isCommanderArmy
-
 	return result
 end
 
@@ -2182,9 +2152,6 @@ function widget:SetConfigData(data)
 	end
 	if data.widgetConfig then
 		settings.widgetConfig = data.widgetConfig
-	end
-	if data.isCommanderArmy then
-		settings.isCommanderArmy = data.isCommanderArmy
 	end
 	if data.oneTimeEcostatsEnableDone then
 		settings.oneTimeEcostatsEnableDone = data.oneTimeEcostatsEnableDone
